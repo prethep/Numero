e31124fc201849e44eca79157d756f93d153fdd8
Prethep Paramananthan <prethep93@gmail.com>
2019-11-28 14:17:03 +0100

    typo fix in ci.sh

--- ci.sh
+++ ci.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-set -e
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination generic/platform=iOS -configuration Release build CODE_SIGNING_ALLOWED=NO
